07ff8965b30e6273a9093e0dc82796e9ebe78fb4
vinandra <vinandra207@gmail.com>
2022-02-19 23:11:52 +0700

    first commit

---
+++ svelte.config.js
@@ -0,0 +1,22 @@
+import adapter from '@sveltejs/adapter-vercel';
+import preprocess from 'svelte-preprocess';
+import WindiCSS from 'vite-plugin-windicss';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: [preprocess()],
+
+	kit: {
+		adapter: adapter(),
+
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+		vite: {
+			plugins: [WindiCSS()],
+		},
+	},
+};
+
+export default config;
