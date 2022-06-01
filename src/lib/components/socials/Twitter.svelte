9a58652baeffdfafac742232a2f423a12c33587d
ANDRA <80272605+vinandra@users.noreply.github.com>
2022-06-01 18:51:25 +0700

    Update Twitter.svelte

--- src/lib/components/socials/Twitter.svelte
+++ src/lib/components/socials/Twitter.svelte
@@ -2,6 +2,6 @@
 	import TwitterIcon from '../icons/Twitter.svelte';
 </script>
 
-<a href="https://twitter.com/vinandra127" target="_blank" rel="noopener noreferrer" class="cursor-pointer">
+<a href="https://twitter.com/v1nandra" target="_blank" rel="noopener noreferrer" class="cursor-pointer">
 	<TwitterIcon />
 </a>
