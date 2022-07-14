2347b098a87b60438002de595c8234c17c337beb
ANDRA <80272605+vinandra@users.noreply.github.com>
2022-07-15 06:58:16 +0700

    Update link

--- src/lib/components/socials/Instagram.svelte
+++ src/lib/components/socials/Instagram.svelte
@@ -2,6 +2,6 @@
 	import InstagramIcon from '../icons/Instagram.svelte';
 </script>
 
-<a href="https://www.instagram.com/v1nandra/" target="_blank" rel="noopener noreferrer" class="cursor-pointer">
+<a href="https://www.instagram.com/shantuuuy/" target="_blank" rel="noopener noreferrer" class="cursor-pointer">
 	<InstagramIcon />
 </a>
