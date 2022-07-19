21409f71cdea3c4944a840b1351760b41772b43d
ANDRA <80272605+Shantuy@users.noreply.github.com>
2022-07-20 06:57:35 +0700

    Update GitHub.svelte

--- src/lib/components/socials/GitHub.svelte
+++ src/lib/components/socials/GitHub.svelte
@@ -2,6 +2,6 @@
 	import GitHubIcon from '../icons/GitHub.svelte';
 </script>
 
-<a href="https://github.com/vinandra" target="_blank" rel="noopener noreferrer" class="cursor-pointer">
+<a href="https://github.com/Shantuy" target="_blank" rel="noopener noreferrer" class="cursor-pointer">
 	<GitHubIcon />
 </a>
